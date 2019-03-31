3215f4ec5d0ed88ccb2fb610041b946b1d532c43
Mohamed Belhadi <belhad.m@gmail.com>
2019-03-31 15:18:38 +0200

    [dev] added header, updated form qb

--- test/test-scheduler.cpp
+++ test/test-scheduler.cpp
@@ -1,3 +1,20 @@
+/*
+ * qb - C++ Actor Framework
+ * Copyright (C) 2011-2019 isndev (www.qbaf.io). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *         limitations under the License.
+ */
+
 #include <gtest/gtest.h>
 #include <qb/main.h>
 #include "../service/Scheduler.h"
@@ -14,9 +31,9 @@ struct TestEvent : qbm::timer::event::Timed
 };
 
 class TestActor : public qb::Actor {
-    qb::ActorId _to;
+    const qb::ActorId _to;
 public:
-    TestActor(uint16_t const core = 0)
+    TestActor(qb::CoreId const core = 0)
             : _to(getServiceId<qbm::timer::service::Tag>(core))
     {}
 
@@ -27,16 +44,16 @@ public:
         e.repeat = 3;
         e.forward = id();
 
-
         return true;
     }
 
-    void on(TestEvent const &event) {
+    void on(TestEvent &event) {
         qb::io::cout() << "event" << event.repeat << std::endl;
         if (!event.repeat) {
             push<qb::KillEvent>(_to);
             kill();
-        }
+        } else
+            event.live(true);
     }
 };
 
